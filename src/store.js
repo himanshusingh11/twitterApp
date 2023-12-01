eaca4abbccf78841cfed9f91e21b3253d7e67ecf
Himanshu Singh <hshimanshusingh001@gmail.com>
2023-12-01 17:58:49 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import tweetReducer from './reducers/tweetReducer';
-
-const store = createStore(tweetReducer, applyMiddleware(thunk));
-
-export default store;
